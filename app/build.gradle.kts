fdb52cbb397ccbb1962bf032bac1e2e3d57e5b11
murjune <sis9221302@gmail.com>
2024-01-22 01:47:09 +0900

    init ktlint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.punch.application)
     alias(libs.plugins.punch.compose)
+    alias(libs.plugins.ktlint)
 //    alias(libs.plugins.google.services)
 //    alias(libs.plugins.app.distribution)
 //    alias(libs.plugins.crashlytics)
