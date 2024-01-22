fd6b25c00aaacb67745b7b0be35b4b2e64946009
GunHyung Ham <54674781+ham2174@users.noreply.github.com>
2024-01-23 03:50:25 +0900

    [Feature] 디자인 시스템 - Icon Button (#13)
    * [init] : 아이콘 추가
    
    * [refactor] : 아이콘 `ic_arrow_rigth_small_24` → `ic_arrow_right_small_24` 로 이름 변경
    
    * [feat] : Icon Asset Object 생성
    
    * [feat] : `FunchIconButton` 제작
    
    * [refactor] : Yellow 500, 600 컬러 변경
    
    * [refactor] : 리뷰 반영

---
+++ core/designsystem/src/main/java/com/moya/funch/component/FunchIconButton.kt
@@ -0,0 +1,109 @@
+package com.moya.funch.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moya.funch.icon.FunchIconAsset
+import com.moya.funch.theme.Gray400
+import com.moya.funch.theme.Gray500
+import com.moya.funch.theme.Yellow500
+
+@Composable
+fun FunchIconLargeButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    @DrawableRes resId: Int,
+    description: String = "",
+    tint: Color,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .size(40.dp)
+            .padding(8.dp)
+            .clickable(
+                onClick = onClick,
+                indication = null,
+                interactionSource = interactionSource,
+            ),
+    ) {
+        Icon(
+            painter = painterResource(id = resId),
+            contentDescription = description,
+            tint = tint,
+        )
+    }
+}
+
+@Composable
+fun FunchIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    @DrawableRes resId: Int,
+    description: String = "",
+    tint: Color,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Icon(
+        modifier = modifier.clickable(
+            onClick = onClick,
+            indication = null,
+            interactionSource = interactionSource,
+        ),
+        painter = painterResource(id = resId),
+        contentDescription = description,
+        tint = tint,
+    )
+}
+
+/*========================== PREVIEW =================================*/
+
+@Preview(showBackground = true, backgroundColor = 0xFF2C2C2C)
+@Composable
+fun FunchLargeIconButtonPreview() {
+    FunchIconLargeButton(
+        onClick = { /*TODO*/ },
+        resId = FunchIconAsset.Search.search_24,
+        modifier = Modifier.background(
+            color = Gray500, shape = RoundedCornerShape(12.dp)
+        ),
+        tint = Yellow500,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FunchMediumIconButtonPreview() {
+    FunchIconButton(
+        onClick = { /*TODO*/ },
+        resId = FunchIconAsset.Search.search_24,
+        tint = Gray400,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FunchSmallIconButtonPreview() {
+    FunchIconButton(
+        onClick = { /*TODO*/ },
+        resId = FunchIconAsset.Search.search_16,
+        tint = Gray400,
+    )
+}
