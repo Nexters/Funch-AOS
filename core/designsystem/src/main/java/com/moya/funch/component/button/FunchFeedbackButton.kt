1da3fe296a1f313fb0cae1aa32dd275fbbff1cfc
murjune <sis9221302@gmail.com>
2024-01-23 21:57:45 +0900

    [feat] FunchFeedBackButton

---
+++ core/designsystem/src/main/java/com/moya/funch/component/button/FunchFeedbackButton.kt
@@ -0,0 +1,50 @@
+package com.moya.funch.component.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moya.funch.theme.FunchTheme
+import com.moya.funch.theme.Gray900
+
+@Composable
+fun FunchFeedbackButton(
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    FunchSubButton(
+        modifier = Modifier.wrapContentSize(),
+        enabled = enabled,
+        buttonType = FunchButtonType.XSmall,
+        onClick = onClick,
+        text = "피드백 보내기",
+        contentHorizontalPadding = 12.dp
+    )
+}
+
+@Preview(name = "feedbackButton - enabled, disabled", showBackground = true, widthDp = 140)
+@Composable
+private fun Preview1() {
+    FunchTheme {
+        Column(
+            Modifier
+                .background(Gray900)
+                .padding(12.dp), horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            FunchFeedbackButton(
+                onClick = { },
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            FunchFeedbackButton(
+                enabled = false,
+                onClick = { /*TODO*/ },
+            )
+        }
+    }
+}
