586e73c5854e7f972fb69c7fd0aed305d258375b
murjune <sis9221302@gmail.com>
2024-01-25 19:25:15 +0900

    [Chore] add @Immutable annotation

--- core/designsystem/src/main/java/com/moya/funch/component/FunchIconButton.kt
+++ core/designsystem/src/main/java/com/moya/funch/component/FunchIconButton.kt
@@ -9,10 +9,9 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +25,7 @@ import com.moya.funch.theme.Gray400
 import com.moya.funch.theme.Gray500
 import com.moya.funch.theme.Yellow500
 
+@Immutable
 data class FunchIcon(
     @DrawableRes val resId: Int,
     val description: String,
