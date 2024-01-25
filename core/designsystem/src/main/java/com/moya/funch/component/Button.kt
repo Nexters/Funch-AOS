2da83a07647b2edc3fd3bfb443f814d35c8ff9aa
GunHyung Ham <gdreagon@naver.com>
2024-01-25 20:23:05 +0900

    [feat] : @Immutable 어노테이션 추가 - 리뷰 반영

--- core/designsystem/src/main/java/com/moya/funch/component/Button.kt
+++ core/designsystem/src/main/java/com/moya/funch/component/Button.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,6 +54,7 @@ enum class FunchButtonType(val shape: Shape, val contentVerticalPadding: Dp, val
     XSmall(RoundedCornerShape(12.dp), 8.dp, funchTypography.b);
 }
 
+@Immutable
 data class FunchIcon(
     @DrawableRes val resId: Int,
     val description: String,
