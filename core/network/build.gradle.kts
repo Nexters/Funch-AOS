d35c5a66dc9569686e3e18c96c63e5e41823cb45
murjune <sis9221302@gmail.com>
2024-01-29 04:54:49 +0900

    [build] ktlint 적용

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -7,6 +7,8 @@ android {
 }
 
 dependencies {
+    implementation(projects.core.datastore)
+
     implementation(libs.bundles.retrofit)
     implementation(platform(libs.okhttp.bom))
     implementation(libs.okhttp.logging.interceptor)
