1b78f98fff3b8b8bb0b8fab285c1d732d3bbd567
murjune <sis9221302@gmail.com>
2024-01-29 05:02:56 +0900

    [build] set ktlint, BASE_URL

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,9 +1,28 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
 plugins {
+    alias(libs.plugins.ktlint)
     alias(libs.plugins.funch.android.library)
 }
 
+val properties = Properties().apply {
+    load(rootProject.file("local.properties").inputStream())
+}
 android {
     namespace = "com.moja.funch.network"
+
+    buildTypes {
+        // getByName("release") {
+        //     buildConfigField(
+        //         "String", "FUNCH_BASE_URL", properties.getProperty("FUNCH_BASE_URL")
+        //     )
+        // }
+        getByName("debug") {
+            buildConfigField(
+                "String", "FUNCH_DEBUG_BASE_URL", properties.getProperty("FUNCH_DEBUG_BASE_URL")
+            )
+        }
+    }
 }
 
 dependencies {
