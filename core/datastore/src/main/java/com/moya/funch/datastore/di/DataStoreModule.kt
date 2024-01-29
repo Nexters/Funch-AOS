c6255b5161eb0d5a3bead75243464fbf731b6a19
JUNWON LEE <87055456+murjune@users.noreply.github.com>
2024-01-30 01:43:08 +0900

    [feat] dataStore 모듈 초기 세팅 (#23)
    * [build] kotlin-Serialization id 수정
    
    * [init] network 모듈
    
    * [build] hilt : migrate from kapt to ksp
    
    * [build] kotlin config
    
    * [build] testing 모듈에 JVM 플러그인 적용
    
    * [build] ksp, hilt verson up
    
    * [build] Build-logic refactoring
    
    * [init] dataStore Module
    
    * [feat] UserCodeDataStore
    
    * [feat] DataStoreModule
    
    * [refactor] Preference 만드는 로직 분리
    
    * [build] ktlint 적용

---
+++ core/datastore/src/main/java/com/moya/funch/datastore/di/DataStoreModule.kt
@@ -0,0 +1,31 @@
+package com.moya.funch.datastore.di
+
+import android.content.SharedPreferences
+import com.moya.funch.datastore.DefaultUserCodeDataStore
+import com.moya.funch.datastore.PreferenceFactory
+import com.moya.funch.datastore.UserCodeDataStore
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideAppPreferences(
+        factory: PreferenceFactory,
+    ): SharedPreferences = factory.create()
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    abstract class Binder {
+
+        @Binds
+        abstract fun bindAppPreferences(dataStore: DefaultUserCodeDataStore): UserCodeDataStore
+    }
+}
