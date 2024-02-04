b75772df1e0dca38ee6c15767b47e8b2d4a8d036
murjune <sis9221302@gmail.com>
2024-02-05 01:43:53 +0900

    [feat] Blood enum

--- core/domain/src/main/java/com/moya/funch/entity/Blood.kt
+++ core/domain/src/main/java/com/moya/funch/entity/Blood.kt
@@ -1,11 +1,9 @@
 package com.moya.funch.entity
 
 enum class Blood {
-    A, B, AB, O, IDLE;
-
-    companion object {
-        fun find(name: String): Blood {
-            return requireNotNull(entries.find { it.name == name }) { "Blood : $name not found" }
-        }
-    }
+    A,
+    B,
+    AB,
+    O,
+    IDLE,
 }
