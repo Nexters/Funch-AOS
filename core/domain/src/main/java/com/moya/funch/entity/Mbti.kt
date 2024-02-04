5512347ee4eb9ef0f6d8684304503151f7c39b15
murjune <sis9221302@gmail.com>
2024-02-05 01:43:43 +0900

    [feat] Mbti enum

--- core/domain/src/main/java/com/moya/funch/entity/Mbti.kt
+++ core/domain/src/main/java/com/moya/funch/entity/Mbti.kt
@@ -1,15 +1,21 @@
 package com.moya.funch.entity
 
 enum class Mbti {
-    ENFJ, ENFP, ENTJ, ENTP,
-    ESFJ, ESFP, ESTJ, ESTP,
-    INFJ, INFP, INTJ, INTP,
-    ISFJ, ISFP, ISTJ, ISTP,
-    IDLE;
-
-    companion object {
-        fun find(mbti: String): Mbti {
-            return requireNotNull(entries.find { it.name == mbti }) { "Mbti : $mbti not found" }
-        }
-    }
+    ENFJ,
+    ENFP,
+    ENTJ,
+    ENTP,
+    ESFJ,
+    ESFP,
+    ESTJ,
+    ESTP,
+    INFJ,
+    INFP,
+    INTJ,
+    INTP,
+    ISFJ,
+    ISFP,
+    ISTJ,
+    ISTP,
+    IDLE,
 }
