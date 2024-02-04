f414b5834285c41944c1c9103fc5015633e617c9
murjune <sis9221302@gmail.com>
2024-02-05 01:46:53 +0900

    [feat] MatchingRepository

--- core/domain/src/main/java/com/moya/funch/repository/MatchingRepository.kt
+++ core/domain/src/main/java/com/moya/funch/repository/MatchingRepository.kt
@@ -1,11 +1,10 @@
 package com.moya.funch.repository
 
-import com.moya.funch.entity.match.MatchingResult
+import com.moya.funch.entity.match.Matching
 
 fun interface MatchingRepository {
-
     suspend fun matchProfile(
         userId: String,
         targetCode: String,
-    ): MatchingResult
+    ): Matching
 }
