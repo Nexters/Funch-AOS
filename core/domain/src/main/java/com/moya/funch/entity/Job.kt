6788b914831e241fef7be721283d0a6c73b8ecf4
murjune <sis9221302@gmail.com>
2024-02-05 01:43:12 +0900

    [feat/test] Job enum

--- core/domain/src/main/java/com/moya/funch/entity/Job.kt
+++ core/domain/src/main/java/com/moya/funch/entity/Job.kt
@@ -1,10 +1,13 @@
 package com.moya.funch.entity
 
 enum class Job(val krName: String) {
-    DEVELOPER("개발자"), DESIGNER("디자이너"), IDLE("");
+    DEVELOPER("개발자"),
+    DESIGNER("디자이너"),
+    IDLE("idle"),
+    ;
 
     companion object {
-        fun find(krName: String): Job {
+        fun of(krName: String): Job {
             return requireNotNull(entries.find { it.krName == krName }) { "Job : $krName not found" }
         }
     }
