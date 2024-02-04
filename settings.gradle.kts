223b3c70ade3e7e596dc6ac8ae1c133d1f2b8b42
murjune <sis9221302@gmail.com>
2024-02-05 01:42:46 +0900

    [build] project build.gradle에 domain 의존성추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ include(":core:designsystem")
 include(":core:testing")
 include(":core:network")
 include(":core:datastore")
-
+include(":core:domain")
 // feature
 include(":feature:profile")
 include(":feature:home")
