39034cf97123f200b09db538de5f819c612a9cc3
murjune <sis9221302@gmail.com>
2024-02-08 05:11:46 +0900

    [feat] 프로필 매칭 로직 구현, event 발행 로직 구현

--- feature/home/src/main/java/com/moya/funch/HomeViewModel.kt
+++ feature/home/src/main/java/com/moya/funch/HomeViewModel.kt
@@ -1,10 +1,16 @@
 package com.moya.funch
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
 // @Gun Hyung TODO : 모델 모듈로 분리
 data class HomeModel(
@@ -22,13 +28,17 @@ data class HomeModel(
 }
 
 @HiltViewModel
-internal class HomeViewModel
-@Inject
-constructor(
-    // @Gun Hyung TODO : UseCase 추가
+internal class HomeViewModel @Inject constructor(
+//    private val canMatchProfileUse: CanMatchProfileUseCase,
 ) : ViewModel() {
     private val _homeModel = MutableStateFlow(HomeModel.empty()) // @Gun Hyung TODO : 모델 초기화 필요
     val homeModel = _homeModel.asStateFlow()
+
+    private val _homeErrorMessage: MutableSharedFlow<String> = MutableSharedFlow()
+    val homeErrorMessage = _homeErrorMessage.asSharedFlow()
+
+    private val _matched = MutableStateFlow(false)
+    val matched = _matched.asStateFlow()
 
     init {
         initHome()
@@ -38,9 +48,33 @@ constructor(
         Timber.e("setMatchingCode: $code")
         _homeModel.value = _homeModel.value.copy(
             matchingCode = code.uppercase()
+        )
+    }
+
+    fun matchProfile() {
+        viewModelScope.launch {
+            // @murjune TODO : canMatchProfileUse(userId, targetCode) 로 대체
+//            if (canMatchProfileUse(userId, targetCode)) {
+//            _homeModel.value = _homeModel.value.copy(canMatched = true)
+//            } else {
+//                _homeErrorMessage.emit("매칭할 수 없는 코드입니다.")
+//            }
+            if (homeModel.value.matchingCode.isBlank()) {
+                _homeErrorMessage.emit("매칭할 수 없는 코드입니다.")
+            } else {
+                delay(1000L)
+                _matched.emit(true)
+            }
+        }
+    }
+
+    fun matchDone() {
+        _matched.value = false
+    }
+
     private fun initHome() { // @Gun Hyung TODO : 도메인 완성시 init 함수 제작
-        // setMyCode("u23c")
-        // setViewCount(12)
+        setMyCode("u23c")
+        setViewCount(12)
     }
 
     private fun setMyCode(code: String) { // @Gun Hyung TODO : 도메인에서 .uppercase() 처리
