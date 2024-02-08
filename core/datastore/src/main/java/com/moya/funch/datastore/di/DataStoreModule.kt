71e700bed2470abc60dc648721365a050cf41c46
murjune <sis9221302@gmail.com>
2024-02-09 04:42:45 +0900

    [feat] user 정보 모두 preference에 저장

--- core/datastore/src/main/java/com/moya/funch/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/com/moya/funch/datastore/di/DataStoreModule.kt
@@ -1,10 +1,7 @@
 package com.moya.funch.datastore.di
 
 import android.content.SharedPreferences
-import com.moya.funch.datastore.DefaultUserCodeDataStore
 import com.moya.funch.datastore.PreferenceFactory
-import com.moya.funch.datastore.UserCodeDataStore
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,11 +14,4 @@ object DataStoreModule {
     @Provides
     @Singleton
     fun provideAppPreferences(factory: PreferenceFactory): SharedPreferences = factory.create()
-
-    @Module
-    @InstallIn(SingletonComponent::class)
-    abstract class Binder {
-        @Binds
-        abstract fun bindAppPreferences(dataStore: DefaultUserCodeDataStore): UserCodeDataStore
-    }
 }
