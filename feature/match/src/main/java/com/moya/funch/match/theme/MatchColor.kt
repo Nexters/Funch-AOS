d55cb84724943cd64a3072250f54ee20fc1b071c
murjune <sis9221302@gmail.com>
2024-02-08 18:41:57 +0900

    [feat] MatchColor

---
+++ feature/match/src/main/java/com/moya/funch/match/theme/MatchColor.kt
@@ -0,0 +1,20 @@
+package com.moya.funch.match.theme
+
+import androidx.compose.ui.graphics.Color
+
+// TODO : @murjune : funchtheme color 확정시 삭제
+
+internal val Coral500 = Color(0xFFF86E6F)
+internal val Lemon500 = Color(0xFFFFE83B)
+internal val Lemon600 = Color(0xFFE1CA13)
+internal val Lemon900 = Color(0xFF90720A)
+internal val Yellow500 = Color(0xFFFFD240)
+internal val Yellow600 = Color(0xFFE1B012)
+internal val White = Color(0xFFFFFFFF)
+internal val Gray900 = Color(0xFF151515)
+internal val Gray800 = Color(0xFF242627)
+internal val Gray700 = Color(0xFF2C2C2C)
+internal val Gray600 = Color(0xFF363636)
+internal val Gray500 = Color(0xFF404040)
+internal val Gray400 = Color(0xFF6D6D6D)
+internal val Gray300 = Color(0xFF9B9B9B)
