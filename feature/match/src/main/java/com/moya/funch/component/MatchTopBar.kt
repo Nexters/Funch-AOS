838ee8fc8f637f422700ae9d0d130a9e4f34a77b
murjune <sis9221302@gmail.com>
2024-02-08 17:48:59 +0900

    [feat] MatchTopBar

---
+++ feature/match/src/main/java/com/moya/funch/component/MatchTopBar.kt
@@ -0,0 +1,24 @@
+package com.moya.funch.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.moya.funch.GRAY_400
+import com.moya.funch.icon.FunchIconAsset
+import com.moya.funch.ui.FunchTopBar
+
+@Composable
+internal fun MatchTopBar(onClose: () -> Unit) {
+    FunchTopBar(
+        modifier = Modifier.padding(start = 12.dp, end = 20.dp),
+        enabledTrailingIcon = false,
+        leadingIcon = FunchIcon(
+            resId = FunchIconAsset.Etc.close_24,
+            description = "close",
+            tint = GRAY_400 // @murjune TODO Changed funchTheme.xxx
+        ),
+        onClickLeadingIcon = onClose
+    )
+}
