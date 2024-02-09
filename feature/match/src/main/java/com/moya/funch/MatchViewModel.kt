880bce2b560e1062b4cf609f3911e9777ff0e2b0
murjune <sis9221302@gmail.com>
2024-02-10 05:17:28 +0900

    [chore] @ExperimentalCoroutinesApi 추가

--- feature/match/src/main/java/com/moya/funch/MatchViewModel.kt
+++ feature/match/src/main/java/com/moya/funch/MatchViewModel.kt
@@ -14,6 +14,7 @@ import com.moya.funch.entity.match.Matching
 import com.moya.funch.entity.match.Recommend
 import com.moya.funch.entity.profile.Profile
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
@@ -30,6 +31,7 @@ class MatchViewModel @Inject constructor(
 
     val matchCode: StateFlow<String> = savedStateHandle.getStateFlow(MATCH_CODE, "")
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     val uiState: StateFlow<MatchUiState> = matchCode.mapLatest { code ->
         if (code.isEmpty()) {
             MatchUiState.Loading
