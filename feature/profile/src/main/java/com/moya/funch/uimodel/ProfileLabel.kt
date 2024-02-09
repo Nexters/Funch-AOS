ba87523633219a15a87ffe162c8f086931666f75
GunHyung Ham <gdreagon@naver.com>
2024-02-10 01:50:24 +0900

    [feat] : 프로필 라벨 ui 모델 추가

---
+++ feature/profile/src/main/java/com/moya/funch/uimodel/ProfileLabel.kt
@@ -0,0 +1,10 @@
+package com.moya.funch.uimodel
+
+enum class ProfileLabel(val labelName: String) {
+    JOB("직군"),
+    CLUB("동아리"),
+    MBTI("MBTI"),
+    BLOOD_TYPE("혈액형"),
+    SUBWAY("지하철")
+    ;
+}
