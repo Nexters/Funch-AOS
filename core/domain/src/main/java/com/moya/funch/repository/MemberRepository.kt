858903d78073b7f21c3fac5897222f56c7e26b67
murjune <sis9221302@gmail.com>
2024-02-10 04:22:40 +0900

    [feat] MemberRepository

---
+++ core/domain/src/main/java/com/moya/funch/repository/MemberRepository.kt
@@ -0,0 +1,11 @@
+package com.moya.funch.repository
+
+import com.moya.funch.entity.profile.Profile
+
+interface MemberRepository {
+    suspend fun fetchUserProfile(): Result<Profile>
+
+    suspend fun createUserProfile(profile: Profile): Result<Profile>
+
+    suspend fun fetchMemberProfile(id: String): Result<Profile>
+}
