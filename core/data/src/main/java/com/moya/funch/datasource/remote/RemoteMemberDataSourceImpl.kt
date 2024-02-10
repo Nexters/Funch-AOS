9a69f2a261bd7c0679f99b9ef1628bd8b926faa1
murjune <sis9221302@gmail.com>
2024-02-11 02:39:01 +0900

    [feat] RemoteMemberDataSource - 복구

--- core/data/src/main/java/com/moya/funch/datasource/remote/RemoteMemberDataSourceImpl.kt
+++ core/data/src/main/java/com/moya/funch/datasource/remote/RemoteMemberDataSourceImpl.kt
@@ -1,18 +1,16 @@
 package com.moya.funch.datasource.remote
 
-import com.moya.funch.datastore.UserDataStore
 import com.moya.funch.model.ProfileModel
 import com.moya.funch.model.toModel
 import com.moya.funch.network.service.MemberService
 import javax.inject.Inject
 
 class RemoteMemberDataSourceImpl @Inject constructor(
-    private val memberService: MemberService,
-    private val userDataStore: UserDataStore
+    private val memberService: MemberService
 ) : RemoteMemberDataSource {
-    override suspend fun fetchMemberProfile(): Result<ProfileModel> {
+    override suspend fun fetchMemberProfile(id: String): Result<ProfileModel> {
         return runCatching {
-            memberService.findMemberById(userDataStore.userId)
+            memberService.findMemberById(id)
         }.map { it.data.toModel() }
     }
 }
