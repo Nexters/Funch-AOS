bdab93b1361f8634038c3ee6af669fde5d0935a5
murjune <sis9221302@gmail.com>
2024-02-11 01:03:55 +0900

    [feat] RemoteMatchDataSource 구현

---
+++ core/data/src/main/java/com/moya/funch/datasource/remote/RemoteMatchDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package com.moya.funch.datasource.remote
+
+import com.moya.funch.datastore.UserDataStore
+import com.moya.funch.network.dto.request.MatchingRequest
+import com.moya.funch.network.dto.response.match.MatchingResponse
+import com.moya.funch.network.service.MatchingService
+import javax.inject.Inject
+
+class RemoteMatchDataSourceImpl @Inject constructor(
+    private val matchingService: MatchingService,
+    private val dataStore: UserDataStore,
+) : RemoteMatchDataSource {
+
+    override suspend fun matchProfile(targetCode: String): Result<MatchingResponse> {
+        return runCatching {
+            matchingService.matchProfile(
+                MatchingRequest(
+                    targetCode,
+                    dataStore.userId
+                )
+            )
+        }.map { it.data }
+    }
+}
