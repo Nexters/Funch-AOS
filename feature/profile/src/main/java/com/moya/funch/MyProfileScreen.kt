ff31e13d82f74c8fdf33bdd1f5dfb1b74e50a449
GunHyung Ham <gdreagon@naver.com>
2024-02-12 03:11:05 +0900

    [feat] : 디자인 시스템에 도메인 Entity name 따른 아이콘 찾는 함수 제작

--- feature/profile/src/main/java/com/moya/funch/MyProfileScreen.kt
+++ feature/profile/src/main/java/com/moya/funch/MyProfileScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.moya.funch.common.searchPainter
 import com.moya.funch.component.FunchChip
 import com.moya.funch.entity.profile.Profile
 import com.moya.funch.icon.FunchIconAsset
@@ -129,14 +130,14 @@ private fun UsersDistinct(profile: Profile) {
                         labelValues.forEach { value ->
                             val leadingIcon = when (profileLabel) {
                                 ProfileLabel.JOB, ProfileLabel.CLUB ->
-                                    profileLeadingIcon(value)
+                                    searchPainter(value)
 
                                 else -> null
                             }
                             val trailingIcon = when (profileLabel) {
                                 ProfileLabel.SUBWAY ->
                                     profile.subways.find { it.name == value }?.lines?.map {
-                                        subwayLineIcon(it.name)
+                                        searchPainter(it.name)
                                     }
 
                                 else -> null
@@ -192,46 +193,6 @@ private fun UsersDistinct(profile: Profile) {
         }
     }
 }
-
-@Composable
-private fun profileLeadingIcon(value: String): Painter = when (value) {
-    "개발자" -> painterResource(id = FunchIconAsset.Job.developer_24)
-    "디자인" -> painterResource(id = FunchIconAsset.Job.designer_24)
-    "디자이너" -> painterResource(id = FunchIconAsset.Club.nexters_24)
-    "넥스터즈" -> painterResource(id = FunchIconAsset.Club.nexters_24)
-    "SOPT" -> painterResource(id = FunchIconAsset.Club.sopt_24)
-    "Depromeet" -> painterResource(id = FunchIconAsset.Club.depromeet_24)
-    else -> throw IllegalArgumentException("Unknown job: $value")
-}
-
-@Composable
-private fun subwayLineIcon(line: String): Painter = // @Gun Hyung TODO : 신림역부터 도메인 Entity 추가 되는데로 수정
-    when (line) {
-        "ONE" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_one)
-        "TWO" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_two)
-        "THREE" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_three)
-        "FOUR" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_four)
-        "FIVE" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_five)
-        "SIX" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_six)
-        "SEVEN" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_seven)
-        "EIGHT" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_eight)
-        "NINE" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_nine)
-        "SINBUNDANG" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_shinbundang)
-        "SUIN" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_suinbundang)
-        "AIRPORT" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_airport)
-        "EVERLINE" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_youngin_ever)
-        "GYEONGCHUN" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_gyeongchun)
-        "신림" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_sillim)
-        "경강" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_geonggang)
-        "서해" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_seohae)
-        "경의중앙" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_gyeongui_jungang)
-        "인천1" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_incheon_one)
-        "의정부" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_uijeongbu)
-        "우이신설" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_ui_sinseol)
-        "김포골드라인" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_gimpo_goldline)
-        "인천2" -> painterResource(id = FunchIconAsset.SubwayLine.subway_line_incheon_two)
-        else -> throw IllegalArgumentException("Unknown subway line: $line")
-    }
 
 @Preview(
     showBackground = true,
