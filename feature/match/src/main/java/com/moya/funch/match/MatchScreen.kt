a2f8c9cc8ded8d52d55935220ac8a2a58e97e20e
GunHyung Ham <gdreagon@naver.com>
2024-02-15 20:37:46 +0900

    [feat] : 매칭시 로딩 UI 제작 및 적용

--- feature/match/src/main/java/com/moya/funch/match/MatchScreen.kt
+++ feature/match/src/main/java/com/moya/funch/match/MatchScreen.kt
@@ -1,11 +1,14 @@
 package com.moya.funch.match
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,14 +16,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.moya.funch.component.FunchIcon
+import com.moya.funch.entity.match.Matching
+import com.moya.funch.icon.FunchIconAsset
 import com.moya.funch.match.component.MatchHorizontalPager
 import com.moya.funch.match.component.MatchTopBar
+import com.moya.funch.match.theme.Gray400
 import com.moya.funch.match.theme.Gray900
 import com.moya.funch.match.theme.White
+import com.moya.funch.theme.FunchTheme
+import com.moya.funch.theme.LocalBackgroundTheme
+import com.moya.funch.ui.FunchTopBar
 
 @Composable
 internal fun MatchRoute(onClose: () -> Unit, code: String, matchViewModel: MatchViewModel = hiltViewModel()) {
@@ -31,15 +46,20 @@ internal fun MatchRoute(onClose: () -> Unit, code: String, matchViewModel: Match
         matchViewModel.saveMatchCode(code)
     }
 
-    MatchScreen(
-        onClose = onClose,
-        memberCode = matchCode,
-        matchUiState = uiState
-    )
+    when (uiState) {
+        is MatchUiState.Loading -> LoadingContent(onClose = onClose)
+        is MatchUiState.Error -> ErrorMatchContent(matchCode)
+        is MatchUiState.Success -> {
+            MatchScreen(
+                onClose = onClose,
+                matching = (uiState as MatchUiState.Success).matching
+            )
+        }
+    }
 }
 
 @Composable
-private fun MatchScreen(onClose: () -> Unit, memberCode: String, matchUiState: MatchUiState) {
+private fun MatchScreen(onClose: () -> Unit, matching: Matching) {
     Column(
         modifier = Modifier
             .background(Gray900)
@@ -49,10 +69,7 @@ private fun MatchScreen(onClose: () -> Unit, memberCode: String, matchUiState: M
     ) {
         MatchTopBar(onClose = onClose)
         Spacer(modifier = Modifier.height(8.dp))
-
-        if (matchUiState is MatchUiState.Success) {
-            MatchHorizontalPager(matching = matchUiState.matching)
-        }
+        MatchHorizontalPager(matching = matching)
     }
 }
 
@@ -62,16 +79,69 @@ internal fun ErrorMatchContent(code: String) {
 }
 
 @Composable
-internal fun LoadingContent() {
-    Text(text = "Loading...", color = White)
+internal fun LoadingContent(onClose: () -> Unit) {
+    val lottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.funch_character_loading
+        )
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        FunchTopBar(
+            modifier = Modifier.padding(start = 12.dp),
+            trailingIcon = null,
+            leadingIcon = FunchIcon(
+                resId = FunchIconAsset.Arrow.arrow_left_small_24,
+                description = "",
+                tint = Gray400
+            ),
+            onClickLeadingIcon = onClose
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(
+                space = 12.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            LottieAnimation(
+                modifier = Modifier.size(200.dp),
+                composition = lottieComposition,
+                iterations = LottieConstants.IterateForever
+            )
+            Text(
+                text = stringResource(id = R.string.match_loading_title),
+                color = White,
+                style = FunchTheme.typography.t2
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true, name = "match screen")
+@Preview(showBackground = true, name = "Success match screen", device = "id:Nexus 6")
 @Composable
-private fun MatchScreenPreview() {
-    MatchScreen(
-        onClose = {},
-        memberCode = "1234",
-        matchUiState = MatchUiState.Loading
-    )
+private fun Preview1() {
+    FunchTheme {
+        Surface(color = LocalBackgroundTheme.current.color) {
+            MatchScreen(
+                onClose = {},
+                matching = Matching()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "Loading match screen", device = "id:Nexus 6")
+@Composable
+private fun Preview2() {
+    FunchTheme {
+        Surface(color = LocalBackgroundTheme.current.color) {
+            LoadingContent(onClose = {})
+        }
+    }
 }
