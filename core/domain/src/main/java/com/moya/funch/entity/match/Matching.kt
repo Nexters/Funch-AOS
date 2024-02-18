b02de9876e32b7232d1259ccbfc495b9136aee64
murjune <sis9221302@gmail.com>
2024-02-18 15:21:20 +0900

    feat(Matching) : job, recommends Matching하는지 체크

--- core/domain/src/main/java/com/moya/funch/entity/match/Matching.kt
+++ core/domain/src/main/java/com/moya/funch/entity/match/Matching.kt
@@ -1,5 +1,10 @@
 package com.moya.funch.entity.match
 
+import com.moya.funch.entity.Blood
+import com.moya.funch.entity.Club
+import com.moya.funch.entity.Job
+import com.moya.funch.entity.Mbti
+import com.moya.funch.entity.SubwayStation
 import com.moya.funch.entity.profile.Profile
 
 data class Matching(
@@ -13,4 +18,36 @@ data class Matching(
             "similarity must be in 0..100"
         }
     }
+
+
+    fun matches(job: Job, recommends: List<Recommend>): Boolean {
+        return recommends.any { recommend ->
+            (job.name == recommend.title) || (job.krName == recommend.title)
+        }
+    }
+
+    fun matches(club: Club, recommends: List<Recommend>): Boolean {
+        return recommends.any { recommend ->
+            (club.name == recommend.title) || (club.label == recommend.title)
+        }
+    }
+
+    fun matches(mbti: Mbti, recommends: List<Recommend>): Boolean {
+        return recommends.any { recommend ->
+            mbti.name == recommend.title
+        }
+    }
+
+    fun matches(blood: Blood, recommends: List<Recommend>): Boolean {
+        return recommends.any { recommend ->
+            (blood.name == recommend.title) || (blood.type == recommend.title)
+        }
+    }
+
+    fun matches(subway: SubwayStation, recommends: List<Recommend>): Boolean {
+        return recommends.any { recommend ->
+            subway.name == recommend.title
+        }
+    }
+
 }
--- core/domain/src/main/java/com/moya/funch/entity/match/Recommend.kt
+++ core/domain/src/main/java/com/moya/funch/entity/match/Recommend.kt
@@ -1,36 +1,5 @@
 package com.moya.funch.entity.match
 
-import com.moya.funch.entity.Blood
-import com.moya.funch.entity.Club
-import com.moya.funch.entity.Job
-import com.moya.funch.entity.Mbti
-
 data class Recommend(
-    private val title: String
-) {
-    fun toJobOrNull(): Job? {
-        return runCatching { Job.of(title) }.getOrNull()
-    }
-
-    fun toClubOrNull(): Club? {
-        return runCatching { Club.of(title) }.getOrNull()
-    }
-
-    fun toBloodOrNull(): Blood? {
-        return runCatching { title.toBlood() }.getOrNull()
-    }
-
-    fun toMbtiOrNull(): Mbti? {
-        return runCatching { Mbti.valueOf(title) }.getOrNull()
-    }
-
-    // TODO : 지하철도 들어가게 되는지는 확인 필요 (지하철 서버 api) 나오면 알듯
-
-    // TODO : 머지되면 삭제
-    private fun String.toBlood(): Blood {
-        val blood = runCatching { Blood.valueOf(this) }
-        if (blood.isSuccess) return requireNotNull(blood.getOrNull())
-        return requireNotNull(Blood.entries.find { it.type == this }) { "Blood : $this not found" }
-    }
-
-}
+    val title: String
+)
