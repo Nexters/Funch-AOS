7c854167ab672352d36e9440f6f2e51be9937a27
murjune <sis9221302@gmail.com>
2024-02-19 20:52:53 +0900

    feat: Splash 구현

--- app/src/main/java/com/moya/funch/MainActivity.kt
+++ app/src/main/java/com/moya/funch/MainActivity.kt
@@ -3,17 +3,37 @@ package com.moya.funch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.moya.funch.splash.LoadingScreen
 import com.moya.funch.theme.FunchTheme
 import com.moya.funch.ui.FunchApp
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             FunchTheme {
-                FunchApp()
+                var showLoading by remember { mutableStateOf(true) }
+
+                LaunchedEffect(Unit) {
+                    delay(1500)
+                    showLoading = false
+                }
+
+                if (showLoading) {
+                    LoadingScreen()
+                } else {
+                    FunchApp()
+                }
             }
         }
     }
